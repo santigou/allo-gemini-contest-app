1b1134443f333f301953145a61235a46a1268551
santigou <santirramos@gmail.com>
2024-07-14 13:57:06 -0500

    Home buttons and Gemini Api Call

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,6 +11,24 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final TextEditingController _controller = TextEditingController();
+  final List<String> randomTextList = [
+    "Quiero prepararme para una entrevista como programador backend junior",
+    "Voy a trabajar como mecero en Italia que necesito saber?",
+    "Quiero aprender ingles para responder correctamente a mi profesor",
+    "Tendre un examen oral en portuges, soy estudiante de universidad"
+  ];
+  String _apiResponse = "";
+
+  void setRandomText(){
+    final random = Random();
+    final randomText = randomTextList[random.nextInt(randomTextList.length)];
+    setState(() {
+      _controller.text = randomText;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,8 +55,94 @@ class _HomeState extends State<Home> {
       ),
 
       //Cuadro de texto y botones
-
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _controller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'What do you want do learn...',
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: setRandomText,
+                    icon: const Icon(Icons.auto_awesome),
+                    label: const Text('Random'),
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  ElevatedButton.icon(
+                    onPressed: () async {
+                      await GeminiApiCall();
+                    },
+                    icon: const Icon(Icons.play_arrow),
+                    label: const Text('Iniciar'),
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 16),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Text(
+                _apiResponse,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              ),
+            ],
+          ),
+        ),
+      ),
 
     );
   }
+
+  //Call gemini api
+  Future<void> GeminiApiCall() async {
+    final String user_prompt = _controller.text;
+    final url = Uri.parse('https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyDDaieCLfRwr9KUyHvC7XTF98Noka1GsLY');
+
+    try {
+
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'contents': [
+            {
+              'parts': [
+                //TODO: reemplazar con un buen prompt que retorne el camino a seguir por el usuario
+                {'text': 'Hello'}
+              ]
+            }
+          ]
+        }),
+      );
+      if (response.statusCode == 200) {
+        final responseData = jsonDecode(response.body);
+        final text = responseData['candidates'][0]['content']['parts'][0]['text'];
+        print(text);
+        setState(() {
+          _apiResponse = text;
+        });
+      } else {
+        _apiResponse = ('Failed to fetch API. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      setState(() {
+        _apiResponse = 'Error: $e';
+      });
+    }
+  }
+
 }
---
+++ lib/services/api_service.dart
@@ -0,0 +1,5 @@
+import 'package:http/http.dart' as http;
+
+class ApiService{
+  //TODO: Pasar llamado a la api para este servicio
+}
