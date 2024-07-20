a996cd73439ddd6c956ddca3654bb71b759d63f6
Daniel Zapata <42874951+DanielZR30@users.noreply.github.com>
2024-07-20 18:21:02 -0500

    Fix(MethodtoGetTheSubtopicFromATopic)

--- lib/domain/repositories/subtopic_repository.dart
+++ lib/domain/repositories/subtopic_repository.dart
@@ -5,6 +5,7 @@ import '../entities/subtopic.dart';
 abstract class SubtopicRepositoryInterface {
   Future<List<Subtopic>> getAllSubtopics();
   Future<Subtopic?> getSubtopicById(String id);
+  Future<List<Subtopic>> getAllSubtopicsByTopic(String topicId);
   Future<void> insertSubtopic(Subtopic subtopic);
   Future<void> updateSubtopic(Subtopic subtopic);
   Future<void> deleteSubtopic(String id);
@@ -16,6 +17,10 @@ class SubtopicRepository implements SubtopicRepositoryInterface {
 
   @override
   Future<List<Subtopic>> getAllSubtopics() => _subtopicDao.getAllSubtopics();
+
+  @override
+  Future<List<Subtopic>> getAllSubtopicsByTopic(String topicId) =>
+      _subtopicDao.getAllSubtopics(topicId: topicId);
 
   @override
   Future<Subtopic?> getSubtopicById(String id) => _subtopicDao.getSubtopicById(id);
